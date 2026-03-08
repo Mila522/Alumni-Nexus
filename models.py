1c8e60d03d4e2a501c3289ad2209ea3ba95af8a2
mila <milatwantwa522@yahoo.com>
2026-03-08 14:50:37 +0200

    Update models.py

--- models.py
+++ models.py
@@ -7,9 +7,9 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     user_id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(100), nullable=False)
-    email=db.Column(db.String(100), unique=True, nullable=False)
-    password=db.Column(db.String(255), nullable=False)
+    name=db.Column(db.String(20), nullable=False)
+    email=db.Column(db.String(30), unique=True, nullable=False)
+    password=db.Column(db.String(8), nullable=False)
 
     role=db.Column(db.String(100), nullable=False)  # 'admin'or'alumni'or'student'
 
